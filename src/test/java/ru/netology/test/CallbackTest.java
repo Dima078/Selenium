e40ab542e07e517c61a623e4ded779da273edcbd
Dmitry Kovtun <mr_write@mail.ru>
2022-03-25 17:14:12 +0300

    test-form

---
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -0,0 +1,53 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void souldSendForm() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id = 'name'] input")).sendKeys("Вася Теркин");
+        driver.findElement(By.cssSelector("[data-test-id = 'phone'] input")).sendKeys("+99999999999");
+        driver.findElement(By.cssSelector("[data-test-id = 'agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+        driver.close();
+    }
+}
